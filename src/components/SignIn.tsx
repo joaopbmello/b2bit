1fa7385b5518499a6e58e0fd6fd34cc34acfef46
João Mello <jpedro2603@gmail.com>
2024-05-18 17:18:25 -0300

    Get data from API

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,13 +1,58 @@
+import { useFormik } from "formik";
 import B2BitLogo from "../assets/B2Bit Logo.png";
 import "../styles/SignIn.css";
+import axios from "axios";
+
+const validate = (values: { email: string; password: string }) => {
+  const errors: { email?: string; password?: string } = {};
+
+  if (!values.email) {
+    errors.email = "Please enter an email address";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Please enter a valid email address";
+  }
+
+  if (!values.password) {
+    errors.password = "Please enter your password";
+  }
+
+  return errors;
+};
 
 const SignIn = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate,
+    validateOnChange: false,
+    validateOnBlur: false,
+    onSubmit: (values) => {
+      axios
+        .post(
+          "https://api.homologation.cliqdrive.com.br/auth/login/",
+          {
+            email: values.email,
+            password: values.password,
+          },
+          {
+            headers: {
+              Accept: "application/json;version=v1_web",
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => console.log(response));
+    },
+  });
+
   return (
     <div className="position-absolute top-50 start-50 translate-middle shadow p-3 signin">
       <div className="d-flex justify-content-center m-4">
         <img src={B2BitLogo} alt="B2Bit Logo" className="w-100" />
       </div>
-      <form>
+      <form onSubmit={formik.handleSubmit} noValidate>
         <div className="mb-4">
           <label htmlFor="email" className="form-label">
             E-mail
@@ -17,7 +62,11 @@ const SignIn = () => {
             type="email"
             className="form-control opacity-75"
             placeholder="@gmail.com"
+            onChange={formik.handleChange}
           />
+          {formik.errors.email && (
+            <p className="text-danger mt-1">{formik.errors.email}</p>
+          )}
         </div>
         <div className="mb-4">
           <label htmlFor="password" className="form-label">
@@ -28,7 +77,12 @@ const SignIn = () => {
             type="password"
             className="form-control opacity-75"
             placeholder="****************"
+            onChange={formik.handleChange}
+            value={formik.values.password}
           />
+          {formik.errors.password && (
+            <p className="text-danger mt-1">{formik.errors.password}</p>
+          )}
         </div>
         <button type="submit" className="btn btn-primary p-2 w-100">
           Sign In
